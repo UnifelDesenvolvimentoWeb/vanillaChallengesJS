b4c34b32d8397e42acbc71c3cd4fee7c99a25514
Larissa-maiara <94052203+Larissa-maiara@users.noreply.github.com>
2024-05-04 16:45:18 -0300

    Update desafio1ao6.js

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -3,6 +3,11 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Desafio 1
 // Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
+  for(let i = 0 ; i < array.length; i +=1){       
+    if (i<array.length){
+    console.log(array[i]);}
+
+  }  
 
 }
 
