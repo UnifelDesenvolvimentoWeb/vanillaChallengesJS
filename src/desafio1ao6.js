f42f2b1ba23fdc3dd6a30d07b8098dc8fadb7ac3
theusseveen22 <mateus.krauss@gmail.com>
2024-09-02 11:06:56 -0300

    Adiciona os seis primeiros exercicios

--- src/desafio1ao6.js
+++ src/desafio1ao6.js
@@ -1,41 +1,30 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Desafio 1
-// Escreva um código que imprima um por um de todos os elementos de um array.
 function printElements(array) {
-
+  array.forEach(element => console.log(element));
 }
 
-// Desafio 2
-// Escreva um código que encontre o maior elemento de um array.
 function biggerNumber(array) {
-
+  console.log(Math.max.apply(null, array));
 }
 
-// Desafio 3
-// Escreva um código que imprima todos os elementos de um array, exceto o primeiro e o último.
-// Caso o array tenha um tamanho menor que 3 imprima a mensagem: 'Tamanho do array inválido'
 function ignoreFirstAndLastElement(array) {
-
+  array.pop()
+  array.shift();
+  array.forEach(element => console.log(element));
 }
 
-// Desafio 4
-// Escreva um código que inverta a ordem dos elementos de um array.
 function invertElementsArray(array) {
-
+  array.reverse().forEach(element => console.log(element));
 }
 
-// Desafio 5
-//  Escreva um código que recebe uma string por parâmetro e imprima a sua versão invertida.
 function invertString(string) {
-
+  console.log(array.reverse());
 }
 
-// Desafio 6
-// Escreva um código que receba um array de números por parâmetro e retorne
-// o array com os elementos duplicados removidos.
 function noDuplicates(array) {
-  
+  let semDuplicados = array.filter((atual, index) => array.indexOf(atual) === index);
+  console.log(semDuplicados)
 }
 
 module.exports = {
