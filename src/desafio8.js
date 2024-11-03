3c5dcffc220343cac3693edbf7d9643360f1907e
Matheus Oliveira <matheus.oliveiradesenv@proton.me>
2024-11-03 11:19:21 -0300

    Desafio 8

--- src/desafio8.js
+++ src/desafio8.js
@@ -1,6 +1,25 @@
 // Desafio 8
-function formatPhoneNumber(telNumber) {
-    
+function formatPhoneNumber(numbers) {
+
+    if (numbers.length !== 11) {
+        return 'Array com tamanho incorreto.';
+    }
+
+    for (let num of numbers) {
+        if (num < 0 || num > 9) {
+            return 'não é possível gerar um número de telefone com esses valores';
+        }
+    }
+
+    const frequency = {};
+    for (let num of numbers) {
+        frequency[num] = (frequency[num] || 0) + 1;
+        if (frequency[num] >= 3) {
+            return 'não é possível gerar um número de telefone com esses valores';
+        }
+    }
+
+    return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
 }
 
 module.exports = {
